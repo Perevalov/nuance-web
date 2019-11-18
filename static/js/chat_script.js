40ab15253e6dc8abb231413de2e33da670fbe749
Aleksandr Perevalov <urcn2@mail.ru>
2019-11-18 20:05:25 +0100

    Update chat_script.js

--- static/js/chat_script.js
+++ static/js/chat_script.js
@@ -82,7 +82,7 @@
 		  }, 2000);
             
             // TODO: outsource $.get address string to config file
-            $.get("http://nuance-core.herokuapp.com/get_answer", {user_text: text}).done(function(data) {
+            $.get("https://nuance-core.herokuapp.com/get_answer", {user_text: text}).done(function(data) {
                 resp = JSON.parse(data);
                 sendMessage(resp['system_text'],'left');
             });
